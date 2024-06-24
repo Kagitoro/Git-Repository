c18db2193a5fca5ca43cc80beb9f4a6d27eaa3f4
Radion Duda <rodion.dooda@ukr.net>
2024-06-24 16:37:50 -0400

    Just basic commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// const example = document.getElementById('example');
+// example.textContent = 'новый текст';
+// example.style.color = 'red';
+
+// document.body.style.backgroundColor = 'white'
+// const newDiv = document.createElement('div');
+// newDiv.textContent = 'Hello';
+// document.body.appendChild(newDiv);
+// console.log(newDiv);
+
+
+// example.remove();
+// newDiv.textContent+=(' World');
+
+// const newDiv2 = document.createElement('div');
+// newDiv2.textContent = 'Omg';
+// console.log(newDiv2);
+
+// newDiv.parentNode.insertBefore(newDiv2,newDiv);
+
+
+
+let body = document.body;
+let btn = document.getElementById('btn');
+console.log(btn);
+btn.textContent = 'Click me';
+btn.style.backgroundColor = 'green';
+btn.style.borderRadius = '20px'
+
+body.style.textAlign = 'center'
+btn.style.marginTop = '50px'
+btn.style.marginTop = '60px'
+
+
+btn.style.marginTop = '70px'
+
+
+
+
